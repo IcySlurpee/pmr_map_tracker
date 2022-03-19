1aeb5e243934c8b7d0a0b957add6bd2d2577b5ed
IcySlurpee <1784495+IcySlurpee@users.noreply.github.com>
2022-03-19 15:49:28 -0700

    feat: adds kkj,hos checks, logic updates

--- scripts/logic_helpers.lua
+++ scripts/logic_helpers.lua
@@ -1,3 +1,5 @@
+-- Underscore prefixed functions return booleans.
+-- For use with other functions because to Lua, '0' is truthy. /shrug
 function has(code, amount)
   local count = Tracker:ProviderCountForCode(code)
   amount = tonumber(amount)
@@ -7,8 +9,13 @@ function has(code, amount)
   return count >= amount
 end
 
-function has_partner(partner)
-  return has(partner)
+function get_object(code)
+  local object = Tracker:FindObjectForCode(code)
+  if object then
+    return object
+  end
+  print("[!ERR] unable to get_object: ", code)
+  return nil
 end
 
 function has_star_spirits(amount)
@@ -17,9 +24,27 @@ function has_star_spirits(amount)
   return count >= amount
 end
 
-function can_access_flower_fields()
+function can_access_star_haven()
+  local amount = get_object("setting_required_spirits").AcquiredCount
+  if has_star_spirits(amount) then
+    return 1
+  end
+  return 0
+end
+
+function can_access_toad_town()
+  if has("setting_start_toad")
+     or (has("setting_start_goomba") and has("flag_beat_goomba_king"))
+     or _can_ride_train()
+     or _can_ride_whale() then
+    return 1
+  else
+    return 0
+  end
+end
+
+function can_plant_seeds()
   if (has("seed1") and has("seed2") and has("seed3") and has("seed4"))
-     or has("setting_open_flower_gate")
      or has("seeds", 4) then
     return 1
   else
@@ -27,8 +52,15 @@ function can_access_flower_fields()
   end
 end
 
+function can_access_flower_fields()
+  if has("setting_open_flower_gate") or has("flag_flower_fields_access") then
+    return 1
+  end
+  return 0
+end
+
 function can_access_blue_house()
-  if (has("spinjump") and has_partner("bombette"))
+  if (has("spinjump") and has("bombette"))
      or has("setting_open_blue_house")
      or has("flag_unlocked_blue_house") then
     return 1
@@ -54,7 +86,7 @@ function can_flip_panels()
 end
 
 function can_see_hidden_blocks()
-  if has("watt") or has("setting_blocksalwaysvisible") then
+  if has("watt") or has("setting_visible_blocks") then
     return 1
   else
     return 0
@@ -68,11 +100,25 @@ function can_ride_train()
     return 0
   end
 end
+function _can_ride_train()
+  if has("setting_start_outpost") or has("flag_bombed_train_rock") then
+    return true
+  else
+    return false
+  end
+end
 
 function can_ride_whale()
   if has("setting_start_yoshi") or has("flag_beat_fuzzipede") then
     return 1
   else
     return 0
+  end
+end
+function _can_ride_whale()
+  if has("setting_start_yoshi") or has("flag_beat_fuzzipede") then
+    return true
+  else
+    return false
   end
 end
